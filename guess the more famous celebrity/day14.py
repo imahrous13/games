886a421c05489c96fe17865f8b02b0139d3c7c55
imahrous13 <imahrous13>
2025-09-18 20:11:41 +0300

    guess more famous

---
+++ guess the more famous celebrity/day14.py
@@ -0,0 +1,44 @@
+import random
+from game_data import data
+
+
+def check_answer(user_guess, a_followers, b_followers):
+    if a_followers > b_followers:
+        return user_guess == "a"
+    else:
+        return user_guess == "b"
+
+
+def format_data(account):
+    account_name = account["name"]
+    account_description = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_description}, from {account_country}"
+
+
+score = 0
+game_should_continue = True
+account_b = random.choice(data)
+
+while game_should_continue:
+    account_a = account_b
+    account_b = random.choice(data)
+    if account_a == account_b:
+        account_b = random.choice(data)
+
+    print(f"Compare A: {format_data(account_a)} ")
+    print(f"Compare B: {format_data(account_b)} ")
+
+    guess = input("choose A or B ").lower()
+
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    if is_correct:
+        score += 1
+        print(f"you're right {score}")
+    else:
+        print(f"wrong {score}")
+        game_should_continue = False
